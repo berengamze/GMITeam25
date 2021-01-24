af508293838097988ffa757ab1b515590e6195ff
Kadir Gok <33297382+akasema@users.noreply.github.com>
2021-01-24 18:29:18 -0500

    second pull req

---
+++ src/test/java/gmibank/GmiBankElements.java
@@ -0,0 +1,9 @@
+package gmibank;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class GmiBankElements {
+    @FindBy(name = "name")
+    public WebElement name;
+}
